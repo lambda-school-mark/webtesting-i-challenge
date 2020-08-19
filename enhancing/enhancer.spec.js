8b4c561238d55ee8b92324d04e7582321baf6356
Mark Pekun <markpekun@Marks-MacBook-Pro.local>
2020-08-19 17:43:13 -0400

    mvp

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,43 @@
-const enhancer = require('./enhancer.js');
-// test away!
+const enhancer = require("./enhancer.js");
+
+const item = {
+  durability: 10,
+  enhancement: 5,
+};
+
+const failTest = {
+  durability: 20,
+  enhancement: 17,
+};
+
+describe("repair test", () => {
+  it("testing repair function", () => {
+    expect(enhancer.repair("shishkabob").durability).toBe(100);
+  });
+});
+
+describe("success test", () => {
+  it("should increase enhancement by 1", () => {
+    expect(enhancer.success(item).enhancement).toBe(6);
+  });
+});
+
+describe("fail test", () => {
+  it("should decrease by 5", () => {
+    expect(enhancer.fail(item).durability).toBe(5);
+  });
+
+  it("should decrease by 10", () => {
+    expect(enhancer.fail(failTest).durability).toBe(10);
+  });
+
+  it("enhancement should decrease by 1", () => {
+    expect(enhancer.fail(failTest).enhancement).toBe(16);
+  });
+});
+
+describe("repair test", () => {
+  it("testing repair function", () => {
+    expect(enhancer.repair("shishkabob").durability).toBe(100);
+  });
+});
